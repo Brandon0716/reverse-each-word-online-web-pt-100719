0b0d894c256f47cd100346867c58faee34d6dd12
essential-robot-4364 <brandon.james414@gmail.com>
2019-10-23 17:37:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,5 @@
-def gimme_bacon
-  puts "Bacon please."
+def reverse_each_word(array)
+  new_array = array.split(" ")
+  reversed_array = new_array.each {|x| x.reverse!}
+  return reversed_array.join(" ")
 end
